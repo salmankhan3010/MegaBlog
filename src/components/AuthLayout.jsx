a766d2f9c0c2bcb43e47096cad409ce9fbf7906e
salmankhan3010 <msalmankhan3010@gmail.com>
2024-06-02 16:24:30 +0530

    login and signup page

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.auth.authStatus);
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
+}
---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import RTE from "./RTE"
+import Input from "./Input";
+import Select from "./Select";
+export {
+  Footer,
+  Header,
+  Logo,
+  Button,
+  Container,
+  LogoutBtn,
+  Select,
+  Input,
+  RTE,
+  Signup,
+  Login,
+  PostForm,
+  PostCard,
+  AuthLayout,
+};
